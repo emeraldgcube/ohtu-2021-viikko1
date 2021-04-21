2c008bb28e1fe5f37c4dcb734cc4e94d3555b697
Topi Pärssinen <parssinentopi@gmail.com>
2021-04-21 21:38:08 +0300

    poistettu virhe2

--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,7 @@ class Varasto:
             self.tilavuus=0
     # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
-        return self.tilavuus - self.saldo 
+        return self.tilavuus - self.saldo
 
     def lisaa_varastoon(self, maara):
         if maara < 0:
